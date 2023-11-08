67b5edf00792d0af90b40dae5423ffe8bfdea957
wielopolski <wielopolskim@gmail.com>
2023-11-08 15:58:04 +0100

    chore: small data refresh

--- db/schema.rb
+++ db/schema.rb
@@ -84,6 +84,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_18_083529) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "first_name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -22,33 +22,26 @@ teams = [
 end
 
 puts '###################### Add users ######################'
-user1 = User.create(email: 'u1@example.com', password: 'password', first_name: 'Ireneusz', last_name: 'Skrobis')
-user2 = User.create(email: 'u2@example.com', password: 'password', first_name: 'Blazej', last_name: 'Kosmowski')
-user3 = User.create(email: 'u3@example.com', password: 'password', first_name: 'Wojtek', last_name: 'Wrona')
-user4 = User.create(email: 'u4@example.com', password: 'password', first_name: 'Marek', last_name: 'Caputa')
-user5 = User.create(email: 'u5@example.com', password: 'password', first_name: 'Mateusz', last_name: 'Matejko')
-user6 = User.create(email: 'admin@example.com', first_name: 'Mateusz', last_name: 'Wieczorek', password: 'password', is_admin: true)
+user1 = User.create(email: 'u1@example.com', password: 'password', first_name: 'Irek', last_name: 'Swirek')
+user2 = User.create(email: 'u2@example.com', password: 'password', first_name: 'Blazej', last_name: 'Karzel')
+user3 = User.create(email: 'u3@example.com', password: 'password', first_name: 'Wojtek', last_name: 'Kortek')
+user4 = User.create(email: 'u4@example.com', password: 'password', first_name: 'Marek', last_name: 'Lalek')
+user5 = User.create(email: 'u5@example.com', password: 'password', first_name: 'Mateusz', last_name: 'Patroneusz')
+user6 = User.create(email: 'admin@example.com', first_name: 'Mateusz', last_name: 'Admin', password: 'password', is_admin: true)
 
 puts '###################### Add rounds ######################'
-round1 = Round.create(number: 1, year: 2022)
-round2 = Round.create(number: 2, year: 2022)
+round1 = Round.create(number: 1, year: 2023)
+round2 = Round.create(number: 2, year: 2023)
 
 puts '###################### Add matches ######################'
-match1 = Match.create(round_id: round1.id, match_date: Date.new(2022, 3, 9), home_team_id: teams['Dolina Skrzycznego'], away_team_id: teams['Bezalin'], home_team_score: 5, away_team_score: 1)
-match2 = Match.create(round_id: round1.id, match_date: Date.new(2022, 3, 9), home_team_id: teams['Aludyne'], away_team_id: teams['Selleo'], home_team_score: 2, away_team_score: 2)
-match3 = Match.create(round_id: round1.id, match_date: Date.new(2022, 3, 9), home_team_id: teams['Aqua'], away_team_id: teams['Abb'], home_team_score: 3, away_team_score: 4)
-match4 = Match.create(round_id: round1.id, match_date: Date.new(2022, 3, 9), home_team_id: teams['Nemak'], away_team_id: teams['Helios'], home_team_score: 2, away_team_score: 10)
-match5 = Match.create(round_id: round1.id, match_date: Date.new(2022, 3, 9), home_team_id: teams['Strumet'], away_team_id: teams['Hutchinson'], home_team_score: 1, away_team_score: 2)
+match1 = Match.create(round_id: round1.id, match_date: Date.new(2023, 11, 9), home_team_id: teams['Dolina Skrzycznego'], away_team_id: teams['Bezalin'], home_team_score: 5, away_team_score: 1)
+match2 = Match.create(round_id: round1.id, match_date: Date.new(2023, 11, 9), home_team_id: teams['Aludyne'], away_team_id: teams['Selleo'], home_team_score: 2, away_team_score: 2)
+match3 = Match.create(round_id: round1.id, match_date: Date.new(2023, 11, 9), home_team_id: teams['Aqua'], away_team_id: teams['Abb'], home_team_score: 3, away_team_score: 4)
+match4 = Match.create(round_id: round1.id, match_date: Date.new(2023, 11, 9), home_team_id: teams['Nemak'], away_team_id: teams['Helios'], home_team_score: 2, away_team_score: 10)
+match5 = Match.create(round_id: round1.id, match_date: Date.new(2023, 11, 9), home_team_id: teams['Strumet'], away_team_id: teams['Hutchinson'], home_team_score: 1, away_team_score: 2)
 
-match6 = Match.create(round_id: round2.id, match_date: Date.new(2022, 3, 16), home_team_id: teams['Helios'], away_team_id: teams['Strumet'], home_team_score: 5, away_team_score: 4)
-match7 = Match.create(round_id: round2.id, match_date: Date.new(2022, 3, 16), home_team_id: teams['Aludyne'], away_team_id: teams['Abb'], home_team_score: 0, away_team_score: 3)
-match8 = Match.create(round_id: round2.id, match_date: Date.new(2022, 3, 16), home_team_id: teams['Bezalin'], away_team_id: teams['Pre Zero'], home_team_score: 4, away_team_score: 1)
-match9 = Match.create(round_id: round2.id, match_date: Date.new(2022, 3, 16), home_team_id: teams['Dolina Skrzycznego'], away_team_id: teams['Selleo'], home_team_score: 6, away_team_score: 8)
-match10 = Match.create(round_id: round2.id, match_date: Date.new(2022, 3, 16), home_team_id: teams['Marion'], away_team_id: teams['Nemak'], home_team_score: 4, away_team_score: 6)
-
-puts '###################### Add bets ######################'
-6.times do |u|
-  10.times do |m|
-    eval %Q{Bet.create(match_id: match#{m+1}.id, user_id: user#{u+1}.id, home_team_score: #{rand(0..5)}, away_team_score: #{rand(0..5)})}
-  end
-end
+match6 = Match.create(round_id: round2.id, match_date: Date.new(2023, 11, 16), home_team_id: teams['Helios'], away_team_id: teams['Strumet'], home_team_score: 5, away_team_score: 4)
+match7 = Match.create(round_id: round2.id, match_date: Date.new(2023, 11, 16), home_team_id: teams['Aludyne'], away_team_id: teams['Abb'], home_team_score: 0, away_team_score: 3)
+match8 = Match.create(round_id: round2.id, match_date: Date.new(2023, 11, 16), home_team_id: teams['Bezalin'], away_team_id: teams['Pre Zero'], home_team_score: 4, away_team_score: 1)
+match9 = Match.create(round_id: round2.id, match_date: Date.new(2023, 11, 16), home_team_id: teams['Dolina Skrzycznego'], away_team_id: teams['Selleo'], home_team_score: 6, away_team_score: 8)
+match10 = Match.create(round_id: round2.id, match_date: Date.new(2023, 11, 16), home_team_id: teams['Marion'], away_team_id: teams['Nemak'], home_team_score: 4, away_team_score: 6)
